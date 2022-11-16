f08e5247bb1962cf453c1265dbe720dc1348db3e
Kaka8548 <katyakatya8548@gmail.com>
2022-11-17 01:33:53 +0400

    Завдання 1: створено події зміни кольору бекграунду по кліку Start

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const bodyEl = document.querySelector('body');
+const startBtnEl = document.querySelector('[data-start]');
+const stopBtnEl = document.querySelector('[data-stop]');
+let timerId = null;
 
+const getRandomHexColor = () => {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+const changeBgColor = () => {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+};
+
+const onStartBtnClick = () => {
+  startBtnEl.disabled = true;
+  changeBgColor();
+  timerId = setInterval(() => {
+    changeBgColor();
+  }, 1000);
+};
+
+const onStopBtnClick = () => {
+  clearInterval(timerId);
+  startBtnEl.disabled = false;
+};
+
+startBtnEl.addEventListener('click', onStartBtnClick);
+stopBtnEl.addEventListener('click', onStopBtnClick);
