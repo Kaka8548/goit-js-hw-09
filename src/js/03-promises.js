6b01fdae6d83c370b383c23bbe2c9c176c339be4
Kaka8548 <katyakatya8548@gmail.com>
2022-11-22 00:19:37 +0400

    Завдання 3: створено генератор промісів, який приймає кількість промісів, перший та наступні затримки. Задіяно бібліотеку notiflix

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,40 @@
+import Notiflix from 'notiflix';
+
+const formEl = document.querySelector('.form');
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        return resolve({ position, delay });
+      }
+      reject({ position, delay });
+    }, delay);
+  });
+}
+
+function onFormElSubmit(event) {
+  event.preventDefault();
+  const { target } = event;
+  const firstDelay = Number(target[0].value);
+  const delayStep = Number(target[1].value);
+  const amount = Number(target[2].value);
+  let delay = firstDelay;
+
+  for (i = 1; i <= amount; i += 1) {
+    let position = i;
+    delay += delayStep;
+    createPromise(position, delay)
+      .then(({ position, delay }) =>
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        )
+      )
+      .catch(({ position, delay }) =>
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`)
+      );
   }
 }
+
+formEl.addEventListener('submit', onFormElSubmit);
